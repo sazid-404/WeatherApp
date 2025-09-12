50463633d3fad681df5cdeae410959d6d7afc49b
sazid-404 <sazzadulislamsazid@gmail.com>
2025-09-12 11:29:28 -0700

    Initial Commit

---
+++ View/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  WeatherApp
+//
+//  Created by Sazzadul Islam on 9/12/25.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
