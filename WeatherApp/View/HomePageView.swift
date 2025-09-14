191940c1d8533761ecc25c341550868e7cf04364
sazid-404 <sazzadulislamsazid@gmail.com>
2025-09-13 22:12:26 -0700

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- WeatherApp/View/HomePageView.swift
+++ WeatherApp/View/HomePageView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 0) {
+                Text("Irving, TX")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding(.top)
+                
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                Text("70° F")
+                    .font(.system(size: 70, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding(.bottom)
+                Text("sunny day")
+                    .font(.system(size: 25, weight: .medium, design: .default))
+                    .padding(.bottom)
+                HStack {
+                    Text("H: 75°").padding(.trailing)
+                    Text("L: 70°")
+                }
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text("Hourly Forecust")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                    }
+                }
+                Spacer()
+                
+               
+            }
+        }
     }
 }
 
