0cbf028285a835af2a2ba1f3f2ce37ae064988d5
sazid-404 <sazzadulislamsazid@gmail.com>
2025-09-14 08:55:21 -0700

    WeatherDataModel created

--- WeatherApp/Model/WeatherDataModel.swift
+++ WeatherApp/Model/WeatherDataModel.swift
@@ -5,4 +5,169 @@
 //  Created by Sazzadul Islam on 9/14/25.
 //
 
+
 import Foundation
+
+// MARK: - WeatherResponse
+struct WeatherResponse: Codable {
+    let lat: Double
+    let lon: Double
+    let timezone: String
+    let timezoneOffset: Int
+    let current: Current
+    let minutely: [Minutely]?
+    let hourly: [Hourly]
+    let daily: [Daily]
+    let alerts: [Alert]?
+
+    enum CodingKeys: String, CodingKey {
+        case lat, lon, timezone
+        case timezoneOffset = "timezone_offset"
+        case current, minutely, hourly, daily, alerts
+    }
+}
+
+// MARK: - Current
+struct Current: Codable {
+    let dt: Int
+    let sunrise: Int?
+    let sunset: Int?
+    let temp: Double
+    let feelsLike: Double
+    let pressure: Int
+    let humidity: Int
+    let dewPoint: Double
+    let uvi: Double
+    let clouds: Int
+    let visibility: Int
+    let windSpeed: Double
+    let windDeg: Int
+    let windGust: Double?
+    let weather: [Weather]
+
+    enum CodingKeys: String, CodingKey {
+        case dt, sunrise, sunset, temp
+        case feelsLike = "feels_like"
+        case pressure, humidity
+        case dewPoint = "dew_point"
+        case uvi, clouds, visibility
+        case windSpeed = "wind_speed"
+        case windDeg = "wind_deg"
+        case windGust = "wind_gust"
+        case weather
+    }
+}
+
+// MARK: - Minutely
+struct Minutely: Codable {
+    let dt: Int
+    let precipitation: Double
+}
+
+// MARK: - Hourly
+struct Hourly: Codable {
+    let dt: Int
+    let temp: Double
+    let feelsLike: Double
+    let pressure: Int
+    let humidity: Int
+    let dewPoint: Double
+    let uvi: Double
+    let clouds: Int
+    let visibility: Int
+    let windSpeed: Double
+    let windDeg: Int
+    let windGust: Double?
+    let weather: [Weather]
+    let pop: Double
+
+    enum CodingKeys: String, CodingKey {
+        case dt, temp
+        case feelsLike = "feels_like"
+        case pressure, humidity
+        case dewPoint = "dew_point"
+        case uvi, clouds, visibility
+        case windSpeed = "wind_speed"
+        case windDeg = "wind_deg"
+        case windGust = "wind_gust"
+        case weather, pop
+    }
+}
+
+// MARK: - Daily
+struct Daily: Codable {
+    let dt: Int
+    let sunrise: Int
+    let sunset: Int
+    let moonrise: Int
+    let moonset: Int
+    let moonPhase: Double
+    let summary: String
+    let temp: Temp
+    let feelsLike: FeelsLike
+    let pressure: Int
+    let humidity: Int
+    let dewPoint: Double
+    let windSpeed: Double
+    let windDeg: Int
+    let windGust: Double
+    let weather: [Weather]
+    let clouds: Int
+    let pop: Double
+    let rain: Double?
+    let uvi: Double
+
+    enum CodingKeys: String, CodingKey {
+        case dt, sunrise, sunset, moonrise, moonset
+        case moonPhase = "moon_phase"
+        case summary, temp
+        case feelsLike = "feels_like"
+        case pressure, humidity
+        case dewPoint = "dew_point"
+        case windSpeed = "wind_speed"
+        case windDeg = "wind_deg"
+        case windGust = "wind_gust"
+        case weather, clouds, pop, rain, uvi
+    }
+}
+
+// MARK: - Temp
+struct Temp: Codable {
+    let day: Double
+    let min: Double
+    let max: Double
+    let night: Double
+    let eve: Double
+    let morn: Double
+}
+
+// MARK: - FeelsLike
+struct FeelsLike: Codable {
+    let day: Double
+    let night: Double
+    let eve: Double
+    let morn: Double
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+// MARK: - Alert
+struct Alert: Codable {
+    let senderName: String
+    let event: String
+    let start: Int
+    let end: Int
+    let description: String
+    let tags: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case senderName = "sender_name"
+        case event, start, end, description, tags
+    }
+}
